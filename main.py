b649183948d466c5b5282df024c25082e98726f7
MargaritaPestretsova <56190485+MargaritaPestretsova@users.noreply.github.com>
2019-10-19 00:38:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,33 @@ color('red', 'yellow','blue','black','brown')
 
 
 
-def traingle (x, y, size, color, angele)
-#TODO: (MargaritaPestretsova)
+def traingle (x1, y1, x2, y2, x3, y3, f_color='white')
+    #TODO: (MargaritaPestretsova)
+    """
+    функция, описывающая треугольник
+    :param x1: position of the first point horizontal 
+    :param y1: position of the first point vertical
+    :param x2: position of the second point horizontal
+    :param y2: position of the second point vertical
+    :param x3: position of the third point horizontal
+    :param y3: position of the third point vertical
+    
 
-to do (MargaritaPestretsova) жги треугольники или круги
+    """
+    speed(7)
+    color(f_color)
+    hideturtle()
+
+    penup()
+    goto(x1, y1)
+
+    pendown()
+    begin_fill()
+    goto(x2, y2)
+    goto(x3, y3)
+    goto(x1, y1)
+    end_fill()
+    penup()
 
 
 
