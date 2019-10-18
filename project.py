960280158ce1141b97518472ab244c72611d86c8
Yegor-Vasilevskii <e.vasilevskii@alumni.nsu.ru>
2019-10-19 00:23:20 +0700

    Initial commit.

--- project.py
+++ project.py
@@ -4,23 +4,23 @@ a = input('')
 print("Введите в какую валюту необходимо перевести", end='\n')
 b = input('')
 print("Введите сумму", end='\n')
-с = input('')
-if a == "RUB"
-    if b == 'USD'
+summa = float(input(''))
+if a == "RUB":
+    if b == 'USD':
         RUB_to_USD = requests.get ('https://api.exchangerate-api.com/v4/latest/RUB').json()['rates']['USD']
-        s_1 = RUB_to_USD * c
-        print(s_1)
-    else b = 'EUR'
+        s_1 = RUB_to_USD * summa
+      print(s_1)
+    else:
         RUB_to_EUR = requests.get ('https://api.exchangerate-api.com/v4/latest/RUB').json()['rates']['EUR']
-        s_2 = RUB_to_EUR * c
-        print(s_2)
-else a = 'KZT'
-    if b = 'USD'
+        s_2 = RUB_to_EUR * summa
+      print(s_2)
+else:
+    if b == 'USD':
         KZT_to_USD = requests.get ('https://api.exchangerate-api.com/v4/latest/KZT').json()['rates']['USD']
-        s_3 = KZT_to_USD * c
-        print(s_3)
-    else b = 'EUR'
+        s_3 = KZT_to_USD * summa
+      print(s_3)
+    else:
         KZT_to_EUR = requests.get ('https://api.exchangerate-api.com/v4/latest/KZT').json()['rates']['EUR']
-        s_4 = KZT_to_EUR * c
-        print(s_4)
+        s_4 = KZT_to_EUR * summa
+      print(s_4)
 pass
